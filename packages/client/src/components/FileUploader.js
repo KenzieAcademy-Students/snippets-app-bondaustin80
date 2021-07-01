8b7d00a9b285eef7ba9ec259493e9635aa22d415
Austin Bond <bondaustin80@gmail.com>
2021-07-01 11:42:50 -0400

    Completed Assessment

--- packages/client/src/components/FileUploader.js
+++ packages/client/src/components/FileUploader.js
@@ -2,10 +2,15 @@ import axios from "utils/axiosConfig"
 import React, { useState } from 'react'
 import { useProvideAuth } from 'hooks/useAuth'
 import { useRequireAuth } from "hooks/useRequireAuth"
+import  Modal  from "react-bootstrap/Modal"
 
 export default function FileUploader() {
     const [newAvatar, setNewAvatar] = useState()
+    const [show, setShow] = useState(false)
     const { state } = useProvideAuth()
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
     const {
         state: { user },
@@ -27,6 +32,7 @@ export default function FileUploader() {
         }).catch((error) => {
             console.log("Error occurred")
         });
+        setShow(false)
     }
 
     function onChange(e) {
@@ -34,10 +40,22 @@ export default function FileUploader() {
     }
 
     return (
-        <form class="uploadForm" action="/upload" method="post" enctype="multipart/form-data">
-            <h1>File Upload</h1>
-            <input type="file" className="custom-file-input" name="myFile" onChange={onChange}/>
-            <button className="upload-button" type="submit" onClick={onFileSubmit}>Upload Avatar</button>
-        </form>
+        <>
+        <button onClick={handleShow}>Change Avatar</button>
+
+        <Modal show={show}>
+            <Modal.Header closeButton onClick={handleClose}>
+                <Modal.Title>File Upload</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <input type="file" className="custom-file-input" name="myFile" onChange={onChange}/>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <button variant="primary" className="upload-button" type="submit" onClick={onFileSubmit}>Upload Avatar</button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
